e5d316194b2ed8a24809a1c4c1637a03db418728
Alexander <www.darmagh@mail.ru>
2016-11-06 15:11:06 +0300

    убрал комментарии, теперь отскок мяча происходит благодоря импульсу физ тела

--- Lab2Arcanoid3D/src/DYN4JBall.java
+++ Lab2Arcanoid3D/src/DYN4JBall.java
@@ -27,54 +27,10 @@ public class DYN4JBall extends Body {
 		return ball;
 	}
 	
-	private Vector2 normalazeVector(Vector2 v1) {
-		return new Vector2(	
-				v1.x / v1.normalize(),
-				v1.y / v1.normalize());
-	}
-	
-	private Vector2 mul(Vector2 v1, Vector2 v2) {
-		return new Vector2(	
-				v1.x / v2.x,
-				v1.y / v2.y);
-	}
-	
 	public void update(GL2 gl) {
 		int ballID = (int) WorldConsts.POSSITION_BALL.getValue();
 		GLBall ball = (GLBall) DialDisplay.sWorld.getBody(ballID);
-		//Vector2 mirrorReflection = null;
-		//Vector2 d = null;
 		ball.render(gl);
-		ball.applyImpulse(15);
-		for(int i = RangesConst.RANGE_BEGIN_FOR_MOVING_PLATFORM.getValue();
-				i < RangesConst.RANGE_END_FOR_BOX.getValue();
-				++i) {
-			
-			if(ball.isInContact(DialDisplay.sWorld.getBody(i))) {
-				
-				/*//mirrorReflection = new Vector2(this.velocity.x , this.velocity.y);
-				d = new Vector2(this.velocity.x , this.velocity.y);
-				System.out.println(d);
-				Vector2 n = normalazeVector(d);
-				System.out.println(n);
-				Vector2 dn = mul(d,n);
-				System.out.println(dn);
-				dn.multiply(2);
-				System.out.println(dn);
-				mirrorReflection = mul(dn,n);
-				mirrorReflection.multiply(2);
-				Vector2 vectorD = d.multiply(2);
-				System.out.println(vectorD);
-				Vector2 vectorD2 = vectorD.multiply(n);
-				System.out.println(vectorD2);
-				//mirrorReflection = d.subtract(vectorD2);
-				//System.out.println(vectorD2);
-				this.velocity =  mirrorReflection;
-				ball.setLinearVelocity(mirrorReflection);*/
-			}
-		}
-		
-		
-		
+		ball.applyImpulse(15);		
 	}
 }
