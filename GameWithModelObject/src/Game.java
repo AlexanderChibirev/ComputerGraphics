bc124f1a6bd0444885339ec3f93a822ccf676c93
Alexander <www.darmagh@mail.ru>
2017-01-07 16:33:19 +0300

    созданы основные классы для отображения черного экрана, создан пустой класс моделей объектов

---
+++ GameWithModelObject/src/Game.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.*;
+
+
+public class Game extends JFrame{
+	private static final long serialVersionUID = -2687152741472560149L;
+	public static Animator animator = null;
+    public static void main(String[] args) {
+        final Game app = new Game();
+        // запуск асинхронной операции
+        SwingUtilities.invokeLater (
+            new Runnable() {
+                public void run() {
+                    app.setVisible(true);
+                }
+            }
+        );
+        
+        SwingUtilities.invokeLater (
+            new Runnable() {
+                public void run() {
+                    animator.start();
+                }
+            }
+        );
+    }
+  
+    public Game() {
+        super("3D function");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DialDisplay display = new DialDisplay();
+        GLCanvas glcanvas =  new GLCanvas();
+        glcanvas.addGLEventListener(display);
+        
+        animator = new Animator(glcanvas);
+        getContentPane().add(glcanvas, BorderLayout.CENTER);
+        setSize(800, 800);
+        centerWindow(this);
+    }
+    
+    private void centerWindow(Component frame) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int windowX = Math.max(0,(screenSize.width - frame.getWidth()) / 2);
+		int windowY = Math.max(0,(screenSize.height - frame.getHeight()) / 2);
+		frame.setLocation(windowX, windowY);
+    }
+}
